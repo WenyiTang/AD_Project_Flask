f24d7b378d2f1b8537005b75a6451348fc298f82
wenyi <wenyi1717@gmail.com>
2022-02-17 23:12:01 +0800

    improve rec logic 17 feb

--- recModel.py
+++ recModel.py
@@ -15,19 +15,23 @@ from sklearn.metrics.pairwise import cosine_similarity
 class recModel :
     def __init__(self) : pass
     def search(self, q, data):
-        df, df_combine_only, combine_arr, df_title_only, title_arr = prepDataBase(data)
+        df, df_combine_only, combine_arr, title_arr, t_arr = prepDataBase(data)
         docs_combine_clean = cleanData(combine_arr)
         docs_title_clean = cleanData(title_arr)
+        docs_t_clean = cleanData(t_arr)
 
-        #Compare title only
+        tfidf_t, feature_vectors_t, vocab_t = prepDBModel(docs_t_clean)
         tfidf_title, feature_vectors_title, vocab_title = prepDBModel(docs_title_clean)
         tfidf_combine, feature_vectors_combine, vocab_combine = prepDBModel(docs_combine_clean)
         query_title_clean, query_combine_clean = cleanGetQuery(q)
-        query_feature_vector_title, query_feature_vector_combine = prepQueryModel(
-            tfidf_title, tfidf_combine, 
+        query_feature_vector_t, query_feature_vector_title, query_feature_vector_combine = prepQueryModel(
+            tfidf_t, tfidf_title, tfidf_combine, 
             query_title_clean, query_combine_clean, 
-            vocab_title, vocab_combine)
-        sorted_series, goodResult = getSimilarity(query_feature_vector_title, feature_vectors_title, query_feature_vector_combine, feature_vectors_combine, df, q)
+            vocab_t, vocab_title, vocab_combine)
+        sorted_series, goodResult = getSimilarity(query_feature_vector_t, feature_vectors_t, 
+                                                query_feature_vector_title, feature_vectors_title, 
+                                                query_feature_vector_combine, feature_vectors_combine, 
+                                                df, q)
         resultFoodEntryId = getResult(sorted_series, df_combine_only, combine_arr)
         resultFoodEntryId.append(goodResult)
         return resultFoodEntryId
@@ -48,13 +52,17 @@ def prepDataBase(data):
     df_combine = df.copy()
     df_combine["combine"] = df_combine["title"] + ", " + df_combine["description"] + ", " + df_combine["track_score"]+ ", " + df_combine["feeling"]
     df_combine["title_desc"] = df_combine["title"] + ", " + df_combine["description"]
+
     #To compare title only
+    df_t_only = df_combine.loc[:,["id", "title"]]
+    t_arr = df_t_only["title"].values
+    #To compare titledesc only
     df_title_only = df_combine.loc[:,["id", "title_desc"]]
     title_arr = df_title_only["title_desc"].values
     #To compare full query
     df_combine_only = df_combine.loc[:, ["id", "combine"]]
     combine_arr = df_combine_only["combine"].values
-    return df, df_combine_only, combine_arr, df_title_only, title_arr
+    return df, df_combine_only, combine_arr, title_arr, t_arr
 
 def preprocess(docs):
     lemmatizer = WordNetLemmatizer()
@@ -64,7 +72,7 @@ def preprocess(docs):
     for doc in docs:
         doc_no_punc = doc.translate(punc)
         words = doc_no_punc.lower().split()
-        words = [lemmatizer.lemmatize(word, 'v')
+        words = [lemmatizer.lemmatize(word, 'n')
                 for word in words if word not in stop_words]
         docs_clean.append(' '.join(words))
 
@@ -100,9 +108,14 @@ def cleanGetQuery(q):
         
     return(cleaned[0], cleaned[1])
 
-def prepQueryModel(tfidf_title, tfidf_combine, query_title_clean, query_combine_clean, vocab_title, vocab_combine):
+def prepQueryModel(tfidf_t, tfidf_title, tfidf_combine, query_title_clean, query_combine_clean, vocab_t, vocab_title, vocab_combine):
 
     #title only
+    query_feature_vector_t = tfidf_t.transform(query_title_clean).toarray()
+    query_t_df = pd.DataFrame(data=query_feature_vector_t,
+                        columns=vocab_t)
+
+    #titledesc only
     query_feature_vector_title = tfidf_title.transform(query_title_clean).toarray()
     query_title_df = pd.DataFrame(data=query_feature_vector_title,
                         columns=vocab_title)
@@ -112,72 +125,147 @@ def prepQueryModel(tfidf_title, tfidf_combine, query_title_clean, query_combine_
     query_combine_df = pd.DataFrame(data=query_feature_vector_combine,
                         columns=vocab_combine)
 
-    return query_title_df, query_combine_df
+    return query_t_df, query_title_df, query_combine_df
 
-def getSimilarity(query_feature_vector_title, feature_vectors_title, query_feature_vector_combine, feature_vectors_combine, df, q):
-    title_similarity = cosine_similarity(query_feature_vector_title, feature_vectors_title)
-    query_similarity = ""
+def getSimilarity(query_feature_vector_t, feature_vectors_t, 
+                    query_feature_vector_title, feature_vectors_title, 
+                    query_feature_vector_combine, feature_vectors_combine, 
+                    df, q):
+
+
+    t_similarity = np.array(cosine_similarity(query_feature_vector_t, feature_vectors_t))
+    title_similarity = np.array(cosine_similarity(query_feature_vector_title, feature_vectors_title))
+    combine_similarity = np.array(cosine_similarity(query_feature_vector_combine, feature_vectors_combine))
+    print(len(t_similarity[0]))
+    print(len(title_similarity[0]))
+    print(len(combine_similarity[0]))
+
+    t_similarity_arr = t_similarity[0]
+    title_similarirty_arr = title_similarity[0]
+    combine_similarity_arr = combine_similarity[0]
+
+    query_similarity = t_similarity[0]
     goodResults = "false"
 
-    combine_similarity = cosine_similarity(query_feature_vector_combine, feature_vectors_combine)
-    
-    #case 1 (check if title only is good enough)
-    if (title_similarity[0].max() > 0.3):
-        goodResults = "true"
-        query_similarity = title_similarity[0]
-        print("word search 1: " + goodResults)
-        print(title_similarity[0].max())
-        
-    #case 2 (check if combine is good enough)
-    elif (combine_similarity[0].max() > 0.3):
-        goodResults = "true"
-        query_similarity = combine_similarity[0]
-        print("word search 2: " + goodResults)
-        print(combine_similarity[0].max())
-    
-    #case 3 (both )
-    else: 
-        goodResults = "false"
-        query_similarity = combine_similarity[0]
-        print("word search 3: " + goodResults)
+    for x in range(len(t_similarity[0])):
+        #title and/or desc good enough
+        if (t_similarity_arr[x] > 0.3 or title_similarirty_arr[x] > 0.3):
+            if (t_similarity_arr[x] > 0.3):
+                query_similarity[x] = 0.5*t_similarity_arr[x] + 0.5*title_similarirty_arr[x]
+            else:
+                query_similarity[x] = title_similarirty_arr[x]
+            query_similarity[x] *= 1.5
+        elif (combine_similarity_arr[x] > 0.3):
+            query_similarity[x] = combine_similarity_arr[x]
+        else: 
+            query_similarity[x] = combine_similarity_arr[x]
 
-    max_sim = query_similarity.max()
-    weight = max_sim/4
+        out = query_similarity[x]
+        weight = query_similarity[x]/4
+        if (weight < 0.1):
+            weight = 0.1
 
-    print("adding weights")
-    for x in range(len(query_similarity)):
         feel = df.loc[x,'feeling'].lower()
         track = df.loc[x,'track_score'].lower()
         if (track == q[2]):
-            query_similarity[x] += weight
+            out += weight
 
-        if ((q[1] != "cry" and q[1] != "pensive") and feel == q[1]):
-            query_similarity[x] += weight
+        if ((q[1] != "cry" and q[1] != "pensive")):
+            out += weight
         #if user is sad, add more weights to joy and happy entries
-        elif (q[1] == "cry" or q[1] == "pensive"):
+        if (q[1] == "cry" or q[1] == "pensive"):
             if (feel == "joy"):
-                query_similarity[x] += (2*weight)
+                out += (2*weight)
             elif (feel == "happy"):
-                query_similarity[x] += weight
+                out += weight
+
+        query_similarity[x] = out
+
+    #case 0 (check if title only is good enough)
+    # if (t_similarity[0].max() > 0.3):
+    #     goodResults = "true"
+    #     query_similarity = t_similarity[0]
+    #     print("word search 0: " + goodResults)
+    #     print(t_similarity[0].max())
+
+    # #case 1 (check if title + desc only is good enough)
+    # if (t_similarity[0].max() > 0.3 or title_similarity[0].max() > 0.3):
+    #     goodResults = "true"
+    #     if (t_similarity[0].max() > 0.3):
+    #         print("title is good")
+    #         query_similarity = 0.5*t_similarity[0] + 0.5*title_similarity[0]
+    #     else:
+    #         print("only desc is good")
+    #         query_similarity = title_similarity[0]
+    #     print("word search 1: " + goodResults)
+    #     query_similarity = query_similarity * 1.5
+    #     print(query_similarity.max())
+        
+    # #case 2 (check if combine is good enough)
+    # elif (combine_similarity[0].max() > 0.3):
+    #     goodResults = "true"
+    #     query_similarity = combine_similarity[0]
+    #     print("word search 2: " + goodResults)
+    #     query_similarity = query_similarity * 1.25
+    #     print(combine_similarity[0].max())
+    
+    # #case 3 (all not enough)
+    # else: 
+    #     goodResults = "false"
+    #     query_similarity = combine_similarity[0]
+    #     print("word search 3: " + goodResults)
+
+    # # query_sim_pass = query_similarity[query_similarity > 0.3]
+    # # query_avg = query_sim_pass.mean()
+    # # print(query_sim_pass)
+    # # print(query_avg)
+    # #max_sim = query_similarity.max()
+    # #print(max_sim)
+    # #weight = query_avg/4
+
+    # print("adding weights")
+    # print(len(query_similarity))
+    # for x in range(len(query_similarity)):
+    #     out = query_similarity[x]
+    #     weight = query_similarity[x]/4
+
+    #     feel = df.loc[x,'feeling'].lower()
+    #     track = df.loc[x,'track_score'].lower()
+    #     if (track == q[2]):
+    #         out += weight
+
+    #     if ((q[1] != "cry" and q[1] != "pensive")):
+    #         out += weight
+    #     #if user is sad, add more weights to joy and happy entries
+    #     if (q[1] == "cry" or q[1] == "pensive"):
+    #         if (feel == "joy"):
+    #             out += (2*weight)
+    #         elif (feel == "happy"):
+    #             out += weight
+
+    #     query_similarity[x] = out
 
     series = pd.Series(query_similarity, index=df.index)
     sorted_series = series.sort_values(ascending=False)
 
+    # if (len(sorted_series[sorted_series > 0.3]) >= 5):
+    #     print("good more than 5")
+    #     goodResults = "true"
     #shuffle top 10 and get 5
-    sorted_series_10 = sorted_series[sorted_series > 0.3].head(8)
-    if (len(sorted_series_10) == 0 or q[0] == ""):
+    sorted_series_10 = sorted_series[sorted_series > 0.45].head(10)
+    print(sorted_series.head(10))
+    if (len(sorted_series_10) == 0):
         goodResults = "false"
-        if (q[0] == "0"):
-            goodResults = "true"
-        sorted_series_10 = sorted_series.head(10)
-        sorted_series_shuff = sorted_series_10.sample(frac=1)
+        sorted_series_shuff = sorted_series.sample(frac=1)
         sorted_series_out = sorted_series_shuff.head(5).sort_values(ascending=False)
     #if dont have at least 5 scores > 0.3, result is not good
     elif (len(sorted_series_10) < 5):
-        goodResults = "false"
+        goodResults = "false" 
         print("more than 5 good: " + goodResults)
         sorted_series_out = sorted_series[sorted_series != 0].head(5).sort_values(ascending=False)
     else:
+        print("good more than 5")
+        goodResults = "true"
         sorted_series_shuff = sorted_series_10.sample(frac=1)
         sorted_series_out = sorted_series_shuff.head(5).sort_values(ascending=False)
 
@@ -186,8 +274,8 @@ def getSimilarity(query_feature_vector_title, feature_vectors_title, query_featu
         goodResults = "true"
 
     #final check if not enough data, goodResults is false
-    # if (len(df) < 50):
-    #     goodResults = "false"
+    if (len(df) < 50):
+        goodResults = "false"
 
     return sorted_series_out, goodResults
 
